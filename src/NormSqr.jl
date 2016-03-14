4a59022fa93a4fc881245a3b532c132053838991
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-14 02:57:49 -0400

    Update NormSqr.jl

--- src/NormSqr.jl
+++ src/NormSqr.jl
@@ -13,6 +13,6 @@ function SumNonNeg{T<:FF{Float64}(a::T, b:T)
     hi,md = eftAdd(a.hi,b.hi)
     c = a.lo + b.lo
     d = lo + c
-    hi, md = eftSumGTE(hi,d)
+    hi, md = eftAddGTE(hi,d)
     FF(hi, md)
 end
