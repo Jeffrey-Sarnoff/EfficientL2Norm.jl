ca06952cbc4b64608bc4c8057c843d915bfe9d24
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-14 02:56:40 -0400

    Update NormSqr.jl

--- src/NormSqr.jl
+++ src/NormSqr.jl
@@ -1,1 +1,18 @@
+# accurate accumlulation
 
+function SumOfSquares(x::Vector{Float64}) 
+    ssqHiLo = zero(FF)
+    for i in 1:n
+        xsqHiLo = eftMul(x[i], x[i])
+        ssqHiLo = SumNonNeg(ssqHiLo, xsqHiLo)
+    end
+    ssqHiLo
+end
+
+function SumNonNeg{T<:FF{Float64}(a::T, b:T)
+    hi,md = eftAdd(a.hi,b.hi)
+    c = a.lo + b.lo
+    d = lo + c
+    hi, md = eftSumGTE(hi,d)
+    FF(hi, md)
+end
