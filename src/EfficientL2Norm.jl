16317f5190ba021a309df5e040e33326490580e6
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-14 02:28:30 -0400

    Update EfficientL2Norm.jl

--- src/EfficientL2Norm.jl
+++ src/EfficientL2Norm.jl
@@ -5,8 +5,8 @@ import Base: abs, sqrt, norm
 export norm, normsqr, netlib_norm
 
 include("Constants.jl")
-include("L2NormSquared.jl")
-include("L2Norm.jl")
+include("NormSqr.jl")
+include("Norm.jl")
 
 
 end # module
