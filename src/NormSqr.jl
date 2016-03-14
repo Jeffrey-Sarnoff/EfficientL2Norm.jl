3bb20038b0a4a5a7e5c7c27340c8fe5a8103cd3e
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-14 03:03:19 -0400

    Update NormSqr.jl

--- src/NormSqr.jl
+++ src/NormSqr.jl
@@ -2,14 +2,14 @@
 
 function SumOfSquares(x::Vector{Float64}) 
     ssqHiLo = zero(FF)
-    for i in 1:n
+    for i in 1:length(x)
         xsqHiLo = eftMul(x[i], x[i])
         ssqHiLo = SumNonNeg(ssqHiLo, xsqHiLo)
     end
     ssqHiLo
 end
 
-function SumNonNeg{T<:FF{Float64}(a::T, b::T)
+function SumNonNeg{T<:FF{Float64}}(a::T, b::T)
     hi,md = eftAdd(a.hi,b.hi)
     c = a.lo + b.lo
     d = lo + c
