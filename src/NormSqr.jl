69c79d8a37a8befd642b5a539e705b52cad65677
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 03:31:34 -0400

    Update NormSqr.jl

--- src/NormSqr.jl
+++ src/NormSqr.jl
@@ -5,7 +5,20 @@ function SumOfSquares(x::Vector{Float64})
         throw(DomainError)
     end
     
-    ssqHiLo = zero(FF)
+    SumSquares(x)
+end
+
+function SumOfSquares(x::Vector{Float32}) 
+    if length(x) >= 699047
+        throw(DomainError)
+    end
+    
+    SumSquares(x)
+end
+
+
+function SumSquares{T<:AbstractFloat}(x::Vector{T})
+    ssqHiLo = zero(FF{T})
     for i in 1:length(x)
         xsqHiLo = eftMul(x[i], x[i])
         ssqHiLo = SumNonNeg(ssqHiLo, xsqHiLo)
@@ -13,7 +26,8 @@ function SumOfSquares(x::Vector{Float64})
     ssqHiLo
 end
 
-function SumNonNeg{T<:FF{Float64}}(a::T, b::T)
+
+function SumNonNeg{T<:AbstractFloat}(a::FF{T}, b::FF{T})
     hi,md = eftAdd(a.hi,b.hi)
     c = a.lo + b.lo
     d = lo + c
